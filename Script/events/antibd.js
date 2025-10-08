74be2dbc01a2ee5413c5c02d78c221daa5620f94
sijukaqueen210-droid <sijukaqueen210@gmail.com>
2025-10-08 23:43:28 +0530

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
+  credits: "𝐍𝐚𝐭𝐚𝐬𝐚 𝐜𝐡𝐚𝐭 𝐛𝐨𝐭",
   description: "Against changing Bot's nickname"
 };
 
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস নাতাসা চেঞ্জ করতে পারবে🖐`}, threadID);
     }  
         }
