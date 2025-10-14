d887f7254b2228f7ed814e1b57c767f418f69507
sijukaqueen210-droid <sijukaqueen210@gmail.com>
2025-10-14 16:27:11 +0530

    Update Helpall.js

--- Script/commands/Helpall.js
+++ Script/commands/Helpall.js
@@ -5,7 +5,7 @@ module.exports.config = {
  name: "helpall",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐒𝐀𝐇𝐔",
+ credits: "𝐏𝐨𝐥𝐚𝐛𝐚𝐳 𝙉𝘼𝙏𝘼𝙎𝘼",
  description: "Displays all available commands in one page",
  commandCategory: "system",
  usages: "[No args]",
@@ -29,17 +29,17 @@ module.exports.run = async function ({ api, event }) {
  const finalText = `╔═══❖ 🌟 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐈𝐒𝐓 🌟 ❖═══╗
 ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
 ╠═════🔰 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎 🔰═════╣
-║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
-║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐏𝐨𝐥𝐚𝐛𝐚𝐳 𝐍𝐚𝐭𝐚𝐬𝐚 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
+║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝙋𝙊𝙇𝘼𝘽𝘼𝙕 𝙉𝘼𝙏𝘼𝙎𝘼
 ║ 📦 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬: ${allCommands.length} 
 ╚═══════════════════════╝`;
 
  
  const backgrounds = [
- "https://i.imgur.com/wu0iDqS.jpeg",
- "https://i.imgur.com/zqsuJnX.jpeg",
- "https://i.imgur.com/Huz3nAE.png",
- "https://i.imgur.com/wu0iDqS.jpeg"
+ "https://i.imgur.com/9m2oKhL.jpeg",
+ "https://i.imgur.com/pHadUrZ.jpeg",
+ "https://i.imgur.com/qntiQgF.jpeg",
+ "https://i.imgur.com/ww93vZI.jpeg"
  ];
  const selectedBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
  const imgPath = __dirname + "/cache/helpallbg.jpg";
